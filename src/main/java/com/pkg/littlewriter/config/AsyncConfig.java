20092fe101f062fa54a49f335231c487937798af
Sy P <yongsa0221@gmail.com>
2024-10-04 21:47:27 +0900

    refactor : asyncExecutor 설정값 수정

--- src/main/java/com/pkg/littlewriter/config/AsyncConfig.java
+++ src/main/java/com/pkg/littlewriter/config/AsyncConfig.java
@@ -11,13 +11,16 @@ import java.util.concurrent.Executor;
 @Configuration
 @EnableAsync(mode= AdviceMode.ASPECTJ)
 public class AsyncConfig {
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 22;
+    private static final int QUEUE_CAPACITY = 4;
 
     @Bean(name="asyncExecutor")
     public Executor asyncThreadTaskExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(50);
-        threadPoolTaskExecutor.setMaxPoolSize(100);
-        threadPoolTaskExecutor.setQueueCapacity(200);
+        threadPoolTaskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        threadPoolTaskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        threadPoolTaskExecutor.setQueueCapacity(QUEUE_CAPACITY);
         threadPoolTaskExecutor.setThreadNamePrefix("thread-pool");
         return threadPoolTaskExecutor;
     }
